be23cbf52a13ac9403cb0fa6485d6041107060eb
James Jensen <jamesjensen1@gmail.com>
2020-08-08 13:42:52 -0600

    remove the bugs

--- test.py
+++ test.py
@@ -4,16 +4,9 @@ Created on Sat Aug  8 11:10:58 2020
 
 """
 
-# bugs
 # Here is a test
 # Testing 1, 2, 3, 4
 print('This is not confusing!')
 print('it does not hurt my head!!!!!')
-# bugs
-
-# I'm adding some stuff here in between
-# bugs
-
 print('I hate this!!!')
 print('This branch hates me!!!!!!!!!!!!!!!!!!!!!')
-# bugs
