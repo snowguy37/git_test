982abfe8153b19433857818fbbb9e09c62009a3b
Scott Jensen <snowguy37@gmail.com>
2020-08-08 12:18:42 -0600

    Commi t

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ Created on Sat Aug  8 11:10:58 2020
 # Here is a test
 # Testing 1, 2, 3, 4
 print('this is confusing')
-print('it hurts my head!!!!!')
-print('I hate this branch!!!')
-print('This branch hates you!!!')
+print('it hurts!!!!!')
+print('I hate this!!!')
+print('This branch hates me!!!!!!!!!!!!!!!!!!!!!1')
