a637d159e7fcb6b5049ffadfdb700875ee4ac943
Scott Jensen <snowguy37@gmail.com>
2020-08-11 19:10:09 -0600

    Playing with vsCode

---
+++ Untitled.py
@@ -0,0 +1,26 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+
+# %%
+import numpy as np
+
+
+# %%
+o = np.ones(24)
+
+
+# %%
+print(o): print(o)
+
+# %% [markdown]
+# ## Here I add text!
+
+# %%
+#Awesome!
+
+
+print(o)
+
+
+
+
